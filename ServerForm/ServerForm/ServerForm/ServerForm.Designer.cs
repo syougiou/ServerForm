32a3a9a164856b127cd1b9871d4690f1d1539e5f
Araya515 <tomoya5.15@me.com>
2022-03-13 17:29:05 +0900

    ServerForm

---
+++ ServerForm/ServerForm/ServerForm/ServerForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace ServerForm
+{
+    partial class servertform
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(37, 40);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(170, 23);
+            this.StartButton.TabIndex = 2;
+            this.StartButton.Text = "サーバー起動開始";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // servertform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(247, 108);
+            this.Controls.Add(this.StartButton);
+            this.MaximizeBox = false;
+            this.Name = "servertform";
+            this.ShowIcon = false;
+            this.Text = "ServerForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button StartButton;
+    }
+}
+
---
+++ ServerForm/ServerForm/ServerForm/ServerForm.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServerForm
+{
+    public partial class servertform : Form
+    {
+        //「netshの登録」の参考リンク
+        //https://yryr.me/programming/csharp/httplistener-url-anything.html
+
+        //コマンドプロンプト環境netshの実行(管理者権限）
+
+        //URLの登録
+        //netsh http add urlacl url=http://localhost:8080/ user=Everyone
+
+        //URLの削除
+        //netsh http delete urlacl url = http://localhost:8080/
+
+        public servertform()
+        {
+            InitializeComponent();
+        }
+
+        bool StartButton_flg = false;
+        HttpListener listener = null;
+
+        private async void StartButton_Click(object sender, EventArgs e)
+        {
+            if (StartButton_flg == false)
+            {
+                StartButton.Text = "停止";
+                StartButton_flg = true;
+                await Task.Run(()=>{ ListenerStart("http://localhost:8080/"); });
+            }
+            else
+            {
+                StartButton.Text = "開始";
+                StartButton_flg = false;
+            }
+        }
+
+        public bool ListenerStart(string prefixes)
+        {
+            if (!HttpListener.IsSupported)
+            {
+                Console.WriteLine("HttpListenerがサポートされていません。");
+                return false;
+            }
+            // URIプレフィックスが必要です。
+            // for example "http://contoso.com:8080/".
+            if (prefixes == null || prefixes.Length == 0)
+            {
+                return false;
+            }
+
+            //リスナーを作成します。
+            if (listener == null)
+            {
+                listener = new HttpListener();
+
+                // プレフィックスを追加します。
+                listener.Prefixes.Add(prefixes);
+            }
+
+            listener.Start();
+            Console.WriteLine("Listening...");
+
+            // GetContextメソッドは、要求を待機している間はブロックします。
+            HttpListenerContext context = listener.GetContext();
+            HttpListenerRequest request = context.Request;
+            // 応答オブジェクトを取得します。
+            HttpListenerResponse response = context.Response;
+            // 応答を作成します。
+            string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+
+            // 応答ストリームを取得し、それに応答を書き込みます
+            response.ContentLength64 = buffer.Length;
+            System.IO.Stream output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+
+            // 出力ストリームを閉じる必要があります。
+            output.Close();
+            listener.Stop();
+            return true;
+        }
+    }
+}
